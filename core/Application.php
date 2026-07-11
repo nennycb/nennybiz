9c0527389426fe4ab206c8e128bac464e31ba151
Zulkifli <nennycb@gmail.com>
2026-07-11 22:54:25 +0800

    feat: create application entry page

---
+++ core/Application.php
@@ -0,0 +1,14 @@
+<?php
+
+class Application
+{
+    public function run(): void
+    {
+        echo "<h1>🚀 NennyBiz</h1>";
+        echo "<hr>";
+        echo "<h3>Version : 0.1 Alpha</h3>";
+        echo "<p>Server Time : " . date('Y-m-d H:i:s') . "</p>";
+        echo "<p>PHP Version : " . PHP_VERSION . "</p>";
+        echo "<p style='color:green;font-weight:bold;'>Application Started Successfully</p>";
+    }
+}
